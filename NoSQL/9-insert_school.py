e3a5dd23df2007dddba12145f4c2a3d6f690a66c
rivero04 <7710@holbertonstudents.com>
2024-04-19 09:10:31 -0700

    return change

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -7,4 +7,4 @@ from pymongo import MongoClient
 def insert_school(mongo_collection, **kwargs):
     """Function that inserts a new document in a collection based on kwargs"""
     result = mongo_collection.insert_one(kwargs)
-    return result_id
+    return result.inserted_id
