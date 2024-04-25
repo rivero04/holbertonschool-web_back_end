57a9aa6eeb3b71d0d4d8b8b2aca9cfdf0ca51a62
rivero04 <7710@holbertonstudents.com>
2024-04-25 08:10:39 -0700

    Task 7 and 8

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,18 @@
+export default class Airport{
+    constructor(name, code) {
+        this._name = name;
+        this._code = code;
+     }
+    
+    get name() {
+        return this._name;
+     }
+    
+    get code() {
+        return this._code;
+     }
+
+    toString() {
+        return this._code;
+     }
+}
