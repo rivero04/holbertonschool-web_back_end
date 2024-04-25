758a50cb823257a6bcbad767cb75ed9abc1c93af
rivero04 <7710@holbertonstudents.com>
2024-04-25 07:30:38 -0700

    Task 3 and 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency.js';
+
+export default class Pricing{
+    constructor(amount, currency){
+        this._amount = amount
+        this._currency = currency
+    }
+
+    get amount() {
+        return this._amount;
+     }
+    
+    set amount(newAmount) {
+        this._amount = newAmount;
+     }
+    
+    get currency() {
+        return this._currency;
+     }
+
+    set currency(newCurrency) {
+        this._currency = newCurrency;
+    }
+
+displayFullPrice(){
+    return this._amount + ' ' + this._currency.name + ' (' + this._currency.code + ')';
+}
+
+static convertPrice(amount, convercionRate){
+    return amount * convercionRate
+}
+}
