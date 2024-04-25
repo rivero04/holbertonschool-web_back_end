337e6d85ddeb43fc73304b0c984c061910688b1c
rivero04 <7710@holbertonstudents.com>
2024-04-25 07:56:11 -0700

    Fix task 5 (estoy cansado jefe)

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,15 +1,12 @@
 export default class Building{
     constructor(sqft){
         this._sqft = sqft
+
+        if (this.constructor !== Building && !this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
     }
 
     get sqft(){
         return this._sqft
     }
 
-    static checkImplementation(extendingClass) {
-        if (typeof extendingClass.prototype.evacuationWarningMessage !== 'function') {
-          throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
-    }
 }
