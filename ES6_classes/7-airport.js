8c567fcb691f3d18f1cbc33ddfcaefd0d23b8577
rivero04 <7710@holbertonstudents.com>
2024-04-25 08:18:23 -0700

    Fix task 7(usando la  propiedad especial de JavaScript)

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -4,15 +4,7 @@ export default class Airport{
         this._code = code;
      }
     
-    get name() {
-        return this._name;
-     }
-    
-    get code() {
+    get [Symbol.toStringTag]() {
         return this._code;
-     }
-
-    toString() {
-        return this._code;
-     }
+    }
 }
