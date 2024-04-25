8719d2ce22dc619d67576a63126071322d8f1df5
rivero04 <7710@holbertonstudents.com>
2024-04-25 07:50:13 -0700

    Fix task 5?

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,15 +1,16 @@
 export default class Building{
     constructor(sqft){
         this._sqft = sqft
+
+        if (this.constructor === Building) {
+            throw new Error('Building is an abstract class and cannot be instantiated directly.');
+        }
+        if (typeof this.evacuationWarningMessage !== 'function') {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
     }
 
     get sqft(){
         return this._sqft
-    }
-
-    static checkExtendingClass() {
-        if (this.constructor !== Building && !this.evacuationWarningMessage) {
-          throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
     }
 }
