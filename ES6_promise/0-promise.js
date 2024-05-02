1bc7aca8ad9da7628cfb5502d8940e7e84892444
rivero04 <7710@holbertonstudents.com>
2024-05-02 10:11:08 -0700

    Fix eslint

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,12 +1,11 @@
 // Return a Promise using this prototype function getResponseFromAPI()
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      const check = true; 
-      if (check) {
-        resolve();
-      } else {
-        reject();
-      }
-    });
-  }
-  
+  return new Promise((resolve, reject) => {
+    const check = true;
+    if (check) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+}
