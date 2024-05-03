a2d81ea553403fb38c783a9d12281069a793cb24
rivero04 <7710@holbertonstudents.com>
2024-05-03 05:41:42 -0700

    Task 9

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const result = mathFunction();
+    queue.push(result);
+    queue.push("Guardrail was processed");
+  } catch (error) {
+    queue.push("Error: " + error.message);
+    queue.push("Guardrail was processed");
+  }
+  return queue;
+}
