b093389142d9a283056025f21419558d531dd1ea
rivero04 <7710@holbertonstudents.com>
2024-05-03 05:48:24 -0700

    Fix eslint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,26 +1,22 @@
-import signUpUser from "./4-user-promise.js";
-import uploadPhoto from "./5-photo-reject.js";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(([userResult, photoError]) => {
-      return [
-        {
-          status: "done",
-          value: userResult,
-        },
-        {
-          status: "rejected",
-          value: photoError.message,
-        },
-      ];
-    })
-    .catch((error) => {
-      return [
-        {
-          status: "rejected",
-          value: error.message,
-        },
-      ];
-    });
+    .then(([userResult, photoError]) => [
+      {
+        status: 'done',
+        value: userResult,
+      },
+      {
+        status: 'rejected',
+        value: photoError.message,
+      },
+    ])
+    .catch((error) => [
+      {
+        status: 'rejected',
+        value: error.message,
+      },
+    ]);
 }
--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -4,10 +4,10 @@ export default function guardrail(mathFunction) {
   try {
     const result = mathFunction();
     queue.push(result);
-    queue.push("Guardrail was processed");
+    queue.push('Guardrail was processed');
   } catch (error) {
-    queue.push("Error: " + error.message);
-    queue.push("Guardrail was processed");
+    queue.push(`Error: ${error.message}`);
+    queue.push('Guardrail was processed');
   }
   return queue;
 }
